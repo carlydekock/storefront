ae8758436fa5acba22ab24296f3e71ab5f32648b
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-04-07 17:56:32 -0700

    lab requirements complete

--- src/components/storefront/storefront.js
+++ src/components/storefront/storefront.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 import Categories from './categories.js';
 import Products from './products.js';
-import CurrentCategory from './current-category.js';
 
 export default function Storefront() {
 
   return (
     <>
       <Categories />
-      {/* <CurrentCategory /> */}
       <Products />
     </>
   )
